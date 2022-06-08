c62e5b8f8bdb43a10986f2b560d3a096dd92a2ea
dafen173 <dafen@ukr.net>
2022-06-08 23:56:49 +0300

    keep add Yalantis

--- english/yalantis.js
+++ english/yalantis.js
@@ -27,9 +27,8 @@ Docker and Phantom
 
 
 PAST FORM OF 'BE' (WAS/WERE)
-
 i was on a team of ....
--   i was on a team six Back-end developers
+-   i was on a team of six Back-end developers
 
 i was responsible for/
 We were responsible for
@@ -38,6 +37,67 @@ We were responsible for
 ...was related to...
 -   Project was related to health and education
 
+
+
+POWER OF 'HAVE'
+have experience in/width
+-   have experience in Redux and PostgreSQl
+
+have knowledge of
+-   have knowledge of JS and React
+
+have skills of
+-   i have skills of building iOS applications 
+based on Swift
+ 
+
+
+NIGHTMARE OF PREPOSITIONS
+-   good at/bad at
+-   well versed in
+-   responsible for
+-   experience in/width
+-   skills of
+
+After prepositions we use verb + ing ending
+-   I was responsible for writinng unit tests
+-   I have skills of developing mobile applications
+
+
+
+ПОДВЕСТИ ИТОГ МОЖНО С ПОМОЩЬЮ МОДАЛЬНОГО ГЛАГОЛА
+-   So I can build applications based on NodeJS and React.
+I can understand and debug large and complex code.
+
+
+PATTERN
+
+I have ______ years eperience with ______
+I am strong in _______
+On my last project my role was _______
+I have skills in ______, and have experience of using_____
+Also, I have expertise in **у меня есть опыт в**______
+I can ______
+
+
+
+МОЖНО ИСПОЛЬЗОВАТЬ ТРЕБОВАНИЯ ИЗ ТЕКТСТА ВАКАНСИИ КАК 
+БАЗУ СЛОВ ДЛЯ РАССКАЗА О СВОЕМ ОПЫТЕ
+
+
+
+BAD TRICK OF 'МНЕ НРАВИТСЯ'/'Я ХОЧУ'
+мне нравится = I like
+заменить на
+I prefer to
+-   I prefer to work with a team
+-   I enjoy working with
+
+я хочу = I want to
+заменить на
+I would like to
+-   I would like to ask about project
+-   I would like to know more about company
 
 
 
