69ea7fe92f963f398edcaa63ceab4fd92dc3eaa5
Alex <dafen@ukr.net>
2022-06-19 11:36:44 +0300

    add favorite books

--- english/about-me.js
+++ english/about-me.js
@@ -34,7 +34,13 @@ National Technical University of Ukraine “Igor Sikorsky Kyiv Polytechnic Insti
 Department of Sound Engineering and Information Registration
 
 
+
+
+
 ПРО СЕБЯ - ОСНОВНОЙ ШАБЛОН
+
+tell us a little about yourself?
+
 My name is Alexander.
 For more than 10 years, I keep working in a company related to the distribution of AV equipment.
 About three years ago, I started to learn HTML and CSS using the HTML ACADEMY resource. Then I studied JS at FreeCodeCamp. 
@@ -47,30 +53,120 @@ Express, PostgreSQL and Redux.
 
 This is a site with two pages. One with a list of users, and the second with a list of user groups. On both of these lists, you can perform KRUD operations.
 
-When changing the list of groups, on the groups page, the list of groups should immediately change when creating a user, on the users page.
-And if at least one user is added to the group, then this group cannot be deleted.
+When changing the list of groups, on the groups page, the list of groups 
+should immediately change when creating a user, on the users page.
+And if at least one user is added to the group, then this group 
+cannot be deleted.
 
-this is the main information about me at the moment in the context of front-end development
-
-
-МОЙ ОБЫЧНЫЙ ДЕНЬ
-Если это обычный будний день, то он начинается с умывания и приготовления завтрака жене, детям и себе. 
 **
-В последнее время дети учатся удалённо или находятся на каникулах , как сейчас 
-А Жена еще спит в такое время,  она удалённо работает.
-Поэтому в основном я только делаю им всем чай, чтобы он остыл когда они проснутся.
+Поясните, что именно вы тут имели ввиду?
+
+При создании нового пользователя надо в определенном поле указать группу 
+к которой он будет относится. Это организовано с помощью элемента ВЫБОР.
+Так вот список груп в этом элементе должен сразу меняться относительно 
+изменений списка груп на странице груп.
+как то так...
+
+Explain what exactly you mean here?
+
+When creating a new user, you must specify a group in a certain field
+to which it will apply. This is organized using the SELECT element.
+So the list of groups in this element should immediately change relative to
+group list changes on the groups page.
+something like this...
+
 **
+
+This is the basic information about me at the moment in the context of 
+front-end development.
+
+
+
+
+ДИАЛОГ №2
+
+Как вы оцениваете свой уровень английского языка?
+
+
+По моему мнению, письменный и разговорный английский я понимаю на среднем уровне.
+Говорить получается слабее, у меня пока что мало практики, но я готов попробовать.
+
+
+ОПИШИТЕ СВОЙ ОБЫЧНЫЙ ДЕНЬ!
+
+Если это обычный будний день, то он начинается с умывания и приготовления завтрака моей жене, моим детям и себе. 
+
+В последнее время дети учатся удалённо или находятся на каникулах , как данный момент.
+А Жена еще спит в такое время, она работает тоже удалённо.
+Поэтому, если честно, на будних днях я только делаю им всем чай, чтобы он остыл когда они проснутся.
+
 Потом я собираюсь и еду на работу.
 На работе я работаю работу.  После нее я заезжаю в мой местный магазин за продуктами.
 Потом, уже у себя дома , я помогаю детям с уроками (в основном с точными науками и с английским)
-После этого продолжаю заниматься своим английским и JS.
+После этого продолжаю заниматься своим английским и изучением веб-разработки.
+
+
+
+
+How do you rate your level of English?
+
+In my opinion, I understand written and spoken English at an intermediate level.
+Speaking is weaker, I have little practice yet, but I'm ready to try.
+
+DESCRIBE YOUR REGULAR DAY!...
+
+If it's a normal weekday, it starts with washing up and making breakfast for my wife, my kids, and myself.
+
+Recently, children are studying remotely or are on vacation, as of the moment.
+And the Wife is still sleeping at such a time, she also works remotely.
+So, to be honest, on weekdays I only make them all tea so that it will cool down when they wake up.
+
+Then I pack up and go to work.
+At work I work work. After that, I stop by my local grocery store.
+Then, already at home, I help children with lessons (mainly with the exact sciences and with English)
+After that, I continue to study my English and study web development.
+
+
+
+
+КАКОЕ ВАШЕ ЛЮБИМОЕ КИНО?
+
+Один из моих любимых фильмов это Хороший, Плохой, Злой!
+Мне всегда нравились вестерны, с их напряжёнными сценами схваток, бескрайними природными видами и атмосферной музыкой.
+
+Также я в восторге от сериала Друзья. Это очень милое шоу с добрым и искраметным юмором. 
+Все его основные герои разные и со своими причудами, но за каждого из них ты переживаешь как за родного.
+
+
+WHAT IS YOUR FAVORITE MOVIE?
+
+One of my favorite movies is The Good, the Bad, the Ugly!
+I've always loved westerns, with their intense fight scenes, endless natural views (scenery) and atmospheric music.
+
+I also love Friends. This is a very sweet show with kind and sparkling humor.
+All of its main characters are different and with their own quirks, but you worry about each of them as a member of your family.
+
+
+
+КАКИЕ ВАШИ ЛЮБИМЫЕ ЛИТЕРАТУРНЫЕ ПРОИЗВЕДЕНИЯ?
+Раньше я достаточно много читал.
+Из того что помню это произведения таких авторов как Джеймс Хедли Чейз, Роберт Толкин, Ник Перумов, Гарри Гаррисон, Роджер Желязны, Марио Пьюзо.
+Любимые романы это Хроники Амбера, Молот и Крест, Стальная Крыса, Крестный отец.
+Ну а потом я увлекся творчеством Стивена Кинга. То что больше всего понравилось это серия Темная башня, Противостояние, Кэрри, Ловец снов.
+
+
+WHAT ARE YOUR FAVORITE WORK OF LITERATURE?
+I used to read quite a lot.
+From what I remember, these are works by such authors as James Hadley Chase, Robert Tolkien, Nick Perumov, Harry Harrison, Roger Zelazny, Mario Puzo.
+Favorite novels are Chronicles of Amber, Hammer and Cross, Steel Rat, Godfather.
+Well, then I became interested in the work of Stephen King. What I liked the most was the series The Dark Tower, Confrontation, Carrie, Dreamcatcher.
+
+
 
 
 Что входит в мои обязанности на работе
 
 
-По моему мнению, письменный и разговорный английский я понимаю на среднем уровне.
-Говорить получается слабее, у меня пока что мало практики, но я готов попробовать.
 
 
 
